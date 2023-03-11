6006d1f2b3c4fb222cd14a88bafbd1d866bcdef4
Jason Williams <jason@centrolutions.com>
2023-03-11 13:35:30 -0600

    add examples of each roofer type

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -1,6 +1,15 @@
 ﻿
 using Inheritance.Roofer;
 
+//var roofer = new AsphaltRoofer();
+
+//remove and apply wood shingles
+//var roofer = new WoodRoofer();
+
+//remove wood and apply asphalt shingles
+//var roofer = new RemoveWoodApplyAsphaltRoofer();
+
+//remove two different layers and apply asphalt
 var roofer = new RemoveWoodAndAsphaltApplyAsphaltRoofer();
 
 roofer.Setup();
